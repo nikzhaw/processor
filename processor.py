9357b1f04fc607f48790efa6ebbeb075f5459473
Niklaus Haenggi <niklaus.haenggi@endress.com>
2022-04-01 20:41:59 +0200

    ready for interupt handler

--- processor.py
+++ processor.py
@@ -12,34 +12,6 @@ import time
 
 
 
-# scheduler
-# updates the command queue with all currently running programs
-
-# var programs = array with all running programs
-# var tasks = how many commands will be executed per period
-
-def update_comands(programs, tasks):
-    length = sum(len(x) for x in programs)
-    i = 0
-    currentProgram = 0
-    programPoint = [0]*len(programs)
-
-    for step in range(length):
-        befehlsspeicher[i] = programs[currentProgram][programPoint[currentProgram]-1]
-
-        print((i + 1) % tasks)
-        if ((i+1) % tasks==0):
-
-            if (currentProgram<len(programs)-1):
-                currentProgram+=1
-            else:
-                currentProgram=0
-        programPoint[currentProgram]+=1
-        print(befehlsspeicher)
-        i+=1
-
-
-
 
 # scheduler
 # updates the command queue with all currently running programs
@@ -61,7 +33,6 @@ def scheduler(programs, tasks):
                     programs[currentProgram].pointer += 1
                     if ((i + 1) % tasks == 0):
                         if (currentProgram < len(programs) - 1):
-                            print(currentProgram)
                             currentProgram += 1
                         else:
                           currentProgram = 0
@@ -90,7 +61,7 @@ def generateProgram(size):
 
 
 class Program:
-  def __init__(self, content , interuptLevel = 0):
+  def __init__(self, content, interuptLevel = 0):
     self.program = content
     self.pointer = 0
     self.interuptLevel  = interuptLevel
@@ -100,14 +71,20 @@ class Program:
 
 
 
+
 def calculate(currentStep, level):
         global cycle
+
         if (interuptLevel < level):
             calculate(interuptLevel)
         if (currentStep==None):
             return
-        currentChache1 = speicher[currentStep[1]]
-        currentChache2 = speicher[currentStep[2]]
+        if(type(currentStep[1]) == int):
+            currentChache1 = speicher[currentStep[1]]
+            currentChache2 = speicher[currentStep[2]]
+        else:
+            currentChache1 = currentStep[1]
+            currentChache2 = currentStep[2]
         if(currentStep[0]=="add"):
             result.append(currentChache1 + currentChache2)
             print("Step: " + str(cycle) + "   Program Number: " + str(currentStep[3]) + "   Program step: " + str(currentStep[4]) + "   Imput: opration = " + currentStep[0] + "  Value 1 = " + str(currentChache1) + " from memory position= " + str(currentStep[1])+ "  Value 2 = " + str(currentChache2) + " from memory position = " + str(currentStep[2]) + "  operation = " + str(currentChache1) + "+" + str(currentChache2) + "=" + str(result[cycle]))
@@ -124,8 +101,9 @@ def calculate(currentStep, level):
             result.append("jump to " + str(currentChache1))
             print("Step: " + str(cycle) + "   Program Number: " + str(currentStep[3]) + "   Program step: " + str(currentStep[4]) + "   Imput: opration = " + currentStep[0] + "  to program position = " + str(currentChache1) + " from memory position= " + str(currentStep[1]))
         elif (currentStep[0] == "print"):
-            result.append("echo: " + str(currentChache1))
-            print("Step: " + str(cycle) + "   Program Number: " + str(currentStep[3]) + "   Program step: " + str(currentStep[4]) + "   Imput: opration = " + currentStep[0] + result)
+            result.append("echo: " + currentChache1)
+            print("Step: " + str(cycle) + "   Program Number: " + str(currentStep[3]) + "   Program step: " + str(currentStep[4]) + "   Imput: opration = " + currentStep[0] + "  echo: " + currentChache1)
+
         elif (currentStep[0] == "start"):
             if (currentChache1 < len(avaiblePrograms)):
                 result.append("program number " + str(currentChache1) + "started")
@@ -146,36 +124,37 @@ def calculate(currentStep, level):
 
 
 def addProgram(program, interupt = 0):
-
-
     if len(program[0])==3:
         k = 0
         for step in program:
             step.append(len(programs))
             step.append(k)
             k += 1
-    pg = Program((program, interupt))
+
+    pg = Program((program))
     avaiblePrograms.append(pg)
-    if (interupt > 0):
-        programs.append(Program(pg))
+    if (interupt < 1):
+        programs.append(pg)
     return pg
 
 
 def startProgram(number):
     pg = avaiblePrograms[number]
-    programs.append(pg, pg.interuptLevel)
+    programs.append(pg)
 
 
 
 
 
-def initProcess():
+
+
+
+
+def startSystem():
     global cycle
     cycle = 0
     global interuptLevel
     interuptLevel = 0
-#    global befehlsspeicher
-#    befehlsspeicher = [None] * 32
     global speicher
     speicher = random.sample(range(100), 32)
     global result
@@ -186,51 +165,94 @@ def initProcess():
     global avaiblePrograms
     avaiblePrograms = []
 
-    generateProgram(16)
-
-    # programs = [[["add", 1 , 2], ["mul", 3 , 4],["add" , 5, 3] , ["add", 1 , 6] , [ "sub", 7 , 4], ["div", 7 , 8] ,["sub", 7 , 4] ,["mul", 7 , 8]],[["add", 11 , 9], ["mul", 13 , 10],["add" , 15, 13] , ["add", 11 , 16] , [ "sub", 13 , 9], ["div", 13 , 16] ,["sub", 11 , 9] ,["mul", 11 , 12]]]
-    program1 = [["add", 1, 2], ["mul", 3, 4], ["add", 5, 3], ["add", 1, 6], ["sub", 7, 4], ["div", 7, 8], ["sub", 7, 4],
-                ["mul", 7, 8]]
-    program2 = [["add", 11, 9], ["mul", 13, 10], ["add", 15, 13], ["add", 11, 16], ["sub", 13, 9], ["div", 13, 16],["sub", 11, 9], ["jump", 0, 0], ["mul", 11, 12]]
-
-
     isr1 = [["print","Fatel Error",0],["print","BlueScreen",0],["print","!!!!!!!!!",0]]
+    addProgram(isr1, 15)
     isr2 = [["print", "Program tries division with 0",0],["print", "Division with 0 is not allowed", 0],["print","Process killed", 0],["print","!!!!!!!!!",0]]
+    addProgram(isr2, 14)
     isr3 = [["print", "Jump adress is out of range", 0],["print", "Jump will not be executed", 0],["print", "Process is continue", 0],["print", "!!!!!!!!!", 0]]
+    addProgram(isr3, 13)
     isr4 = [["print", "Program is not avaible", 0],["print", "Program has to be loaded", 0],["print", "Process is continue", 0],["print", "!!!!!!!!!", 0]]
+    addProgram(isr4, 12)
+    isr5 = generateProgram(10)
+    addProgram(isr5, 11)
 
 
 
-
-
-
-
-
-    programCounter = addProgram(program1)
-    print(programCounter)
-    programCounter = addProgram(program2)
-    print(programCounter)
-   # programCounter = addProgram(program1)
-    print(programCounter)
-
-   # print(befehlsspeicher)
 
     scheduler(programs, 3)
 
-    calculate(0)
 
-    print("finish")
-    print(befehlsspeicher)
+def runPrograms2():
+    time.sleep(1)
+    program1 = generateProgram(10)
+    addProgram(program1)
+    time.sleep(2.5)
+    program2 = generateProgram(10)
+    addProgram(program2)
+    time.sleep(10)
+    program3 = generateProgram(30)
+    time.sleep(1.5)
+    startProgram(1)
+
+
+
+
+def runPrograms():
+    print("eeee")
+    time.sleep(2)
+    startProgram(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    generateProgram(16)
+
+    # programs = [[["add", 1 , 2], ["mul", 3 , 4],["add" , 5, 3] , ["add", 1 , 6] , [ "sub", 7 , 4], ["div", 7 , 8] ,["sub", 7 , 4] ,["mul", 7 , 8]],[["add", 11 , 9], ["mul", 13 , 10],["add" , 15, 13] , ["add", 11 , 16] , [ "sub", 13 , 9], ["div", 13 , 16] ,["sub", 11 , 9] ,["mul", 11 , 12]]]
+    program1 = [["add", 1, 2], ["mul", 3, 4], ["add", 5, 3], ["add", 1, 6], ["sub", 7, 4], ["div", 7, 8], ["sub", 7, 4], ["mul", 7, 8]]
+
+    program3 = generateProgram(10)
+
+
+
+
+
+
+
+    program2 = [["add", 11, 9], ["mul", 13, 10], ["add", 15, 13], ["add", 11, 16], ["sub", 13, 9], ["div", 13, 16],["sub", 11, 9], ["jump", 0, 0], ["mul", 11, 12]]
+
+
+
+
+
+
+
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cpu = threading.Thread(target=initProcess())
-  #  pic = threading.Thread(target=interrupt_controller_thread)
-
+    cpu = threading.Thread(target=startSystem)
+    user = threading.Thread(target=runPrograms)
     cpu.start()
-  #  pic.start()
-  #  initProcess()
+    user.start()
+
+  #  cpu.join()
+  #  user.join()
 
 
 
@@ -245,35 +267,3 @@ if __name__ == '__main__':
 
 
 
-def interrupt_controller_thread():
-  while True:
-    global interrupt
-    time.sleep(interrupt_interval)
-    print("Triggering interrupt")
-    interrupt=True
-
-def cpu_handle_interrupt():
-    global interrupt
-
-    if interrupt == True:
-      print("Woah I got an interrupt")
-      # now act on the interrupt...
-      interrupt = False
-
-def cpu_thread():
-  while True:
-    # Read, Eval, (optional: Print), Loop
-    print("I am reading the next instruction")
-    print("I am evaluating the instruction")
-    time.sleep(1) # python threading doesn't really work
-    cpu_handle_interrupt()
-
-cpu = threading.Thread( target = cpu_thread )
-pic = threading.Thread( target = interrupt_controller_thread )
-
-cpu.start()
-pic.start()
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
